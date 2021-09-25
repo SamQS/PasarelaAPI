d29aa3699f64da5bcb83aacf2712e092239eebe4
GiancarloFernandez <giancarlo_fernandez1@usmp.pe>
2021-09-24 21:34:33 -0500

    add_optional

--- pasarela/src/main/java/com/pasarela/pasarela/repository/PagoRepository.java
+++ pasarela/src/main/java/com/pasarela/pasarela/repository/PagoRepository.java
@@ -3,4 +3,5 @@ import org.springframework.stereotype.*;
 
 public interface PagoRepository extends JpaRepository<Pago, Integer>{
     @Query(value = "SELECT o FROM Pago o WHERE o.numeroTarjeta=?1")
+    Optional<Pago> findByNumeroTarjeta(Integer numeroTarjeta);
 }
