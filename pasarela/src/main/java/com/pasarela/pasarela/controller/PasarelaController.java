a5e50dd32f9e1af8df78823033b042423d617c02
Samuel <diegodsqs2@gmail.com>
2021-09-25 22:05:58 -0500

    Cambios menores

--- pasarela/src/main/java/com/pasarela/pasarela/controller/PasarelaController.java
+++ pasarela/src/main/java/com/pasarela/pasarela/controller/PasarelaController.java
@@ -13,15 +13,19 @@ import java.util.*;
 public class PasarelaController {
 
 private Map<String,Pago> pagos;
+private Map<String,Bancaria> bancarias;
 
     private final PagoRepository pagoData;
+    private final BancariaRepository bancariaData;
     
 
-public PasarelaController(PagoRepository pagoData){
+public PasarelaController(PagoRepository pagoData,BancariaRepository bancariaData){
 
   
   this.pagoData = pagoData;
+  this.bancariaData = bancariaData;
   pagos = new HashMap<String,Pago>();
+  bancarias = new HashMap<String,Bancaria>();
 
 
 
@@ -33,7 +37,7 @@ public ResponseEntity  <Map<String, Pago>> TarjetasRegistradas(){
 
 }
 @PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-public ResponseEntity <String> create(@RequestBody Pago pa){
+public ResponseEntity <String> createTarjeta(@RequestBody Pago pa){
 
   pagoData.save(pa);
   pagoData.flush();
@@ -54,5 +58,38 @@ public ResponseEntity<Pago> TarjetasRegistradass(@PathVariable String id){
         return new ResponseEntity<Pago>(HttpStatus.NOT_FOUND);
     }
 } 
+
+
+
+//BAncaria
+@PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
+public ResponseEntity <String> createCuentaBancaria(@RequestBody Bancaria ba){
+
+    bancariaData.save(ba);
+    bancariaData.flush();
+
+    String id = UUID.randomUUID().toString();
+    ba.setId(id);
+    bancarias.put(id, ba);
+    
+    return new ResponseEntity<String>(id,HttpStatus.CREATED);
+
+}
+@GetMapping(value = "/CuentasB", produces = MediaType.APPLICATION_JSON_VALUE)
+public ResponseEntity  <Map<String, Bancaria>> CuentasBancarias(){
+
+    return new ResponseEntity<Map<String, Bancaria>>(bancarias,HttpStatus.OK);
+
+}
+@GetMapping(value = "/cuentas{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+public ResponseEntity<Bancaria> CuentasBancarias(@PathVariable String id){
+    if(bancarias.containsKey(id)){
+        Bancaria ba = bancarias.get(id);
+        return new ResponseEntity<Bancaria>(ba, HttpStatus.OK);
+    }else{
+        return new ResponseEntity<Bancaria>(HttpStatus.NOT_FOUND);
+    }
+} 
+
 
 }
